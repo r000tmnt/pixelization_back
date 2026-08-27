f0d776841a935d5bf723a18ff31abbdf250ee4a7
ParkCorner <rick.t.yahoocomtw653@gmail.com>
2026-08-27 18:09:46 +0800

    [Create] Pre-defined palettes.

---
+++ config/palette.ts
@@ -0,0 +1,10 @@
+const palettes : Record<string, number[][]> = {
+    gameboy: [
+        [27, 35, 25],
+        [53, 70, 32],
+        [101, 110, 41],
+        [144, 146, 117]
+    ]
+}
+
+export default palettes
